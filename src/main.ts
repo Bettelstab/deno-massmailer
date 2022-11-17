eb9b3d0d1ff839244a7995d7e6e819e8bd8e9155
Maximilian Harz <maxharz@gmail.com>
2022-11-17 17:50:16 +0100

    Read subject and text from input file; read tos from lines of text

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import "https://deno.land/std@0.136.0/dotenv/load.ts";
 
 import { sendMassMail } from "./sendMail.ts";
-// TODO Pass as argument(?), this is a workaround to avoid exposure of addresses (tos.ts is gitignored)
-import { tos } from "./tos.ts";
 
-await sendMassMail(tos, "Anfrage", "Ein Text");
+// TODO Pass as arguments(?), this is a workaround to avoid exposure of addresses (files are gitignored)
+import { subject } from "./input/subject.ts";
+import { text } from "./input/text.ts";
+import { tos } from "./input/tos.ts";
+
+const tosArray = Array.from(new Set(tos.split("\n"))).filter(to => to !== "");
+
+await sendMassMail(tosArray, subject, text);
